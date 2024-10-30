0ec330a744f53e8f271cf236ce40156c593f4cfe
Noubiap Nomsi Roumuald <roumual.noubiap@camcis.org>
2024-10-30 15:51:33 +0100

    configuration eureka service

---
+++ src/test/java/com/eureka_config_service/EurekaConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka_config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
